a82465ce8b15f227c89e564b3999346e2f7f943e
Nathan Woltman <nwoltman@outlook.com>
2019-06-19 10:34:54 -0400

    refactor: Update medley and use the self-request plugin for tests

--- test/test.js
+++ test/test.js
@@ -2,14 +2,22 @@
 
 const assert = require('assert');
 const medley = require('@medley/medley');
+const selfRequest = require('@medley/self-request');
 const cookie = require('..');
+
+function makeApp(cookieOpts) {
+  const app = medley();
+
+  app.register(cookie, cookieOpts);
+  app.register(selfRequest);
+
+  return app;
+}
 
 describe('req.cookies', () => {
 
   it('should be an object of parsed cookies', async () => {
-    const app = medley();
-
-    app.register(cookie);
+    const app = makeApp();
 
     app.get('/', (req, res) => {
       assert.deepStrictEqual(req.cookies, {
@@ -20,13 +28,13 @@ describe('req.cookies', () => {
       res.send('success');
     });
 
-    const res = await app.inject({
+    const res = await app.request({
       url: '/',
       headers: {
         Cookie: 'foo=bar; withSpace=a%20space',
       },
     });
-    assert.strictEqual(res.payload, 'success');
+    assert.strictEqual(res.body, 'success');
   });
 
 });
@@ -35,32 +43,27 @@ describe('req.cookies', () => {
 describe('res.setCookie()', () => {
 
   it('should set a cookie to send to the client', async () => {
-    const app = medley();
-
-    app.register(cookie);
+    const app = makeApp();
 
     app.get('/', (req, res) => {
       res.setCookie('foo', 'bar');
       res.send();
     });
 
-    const res = await app.inject('/');
-    assert.strictEqual(res.headers['set-cookie'], 'foo=bar; Path=/');
+    const res = await app.request('/');
+    assert.deepStrictEqual(res.headers['set-cookie'], ['foo=bar; Path=/']);
   });
 
   it('should set multiple cookies to send to the client', async () => {
-    const app = medley();
-
-    app.register(cookie);
+    const app = makeApp();
 
     app.get('/', (req, res) => {
-      res
-        .setCookie('a', '1')
-        .setCookie('b', '2', {maxAge: 3600, httpOnly: true, secure: true})
-        .send();
+      res.setCookie('a', '1');
+      res.setCookie('b', '2', {maxAge: 3600, httpOnly: true, secure: true});
+      res.send();
     });
 
-    const res = await app.inject('/');
+    const res = await app.request('/');
     assert.deepStrictEqual(res.headers['set-cookie'], [
       'a=1; Path=/',
       'b=2; Max-Age=3600; Path=/; HttpOnly; Secure',
@@ -73,35 +76,30 @@ describe('res.setCookie()', () => {
 describe('res.clearCookie()', () => {
 
   it('should clear a cookie', async () => {
-    const app = medley();
-
-    app.register(cookie);
+    const app = makeApp();
 
     app.get('/', (req, res) => {
       res.clearCookie('foo', 'bar');
       res.send();
     });
 
-    const res = await app.inject('/');
-    assert.strictEqual(
+    const res = await app.request('/');
+    assert.deepStrictEqual(
       res.headers['set-cookie'],
-      'foo=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT'
+      ['foo=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT']
     );
   });
 
   it('should clear multiple cookies', async () => {
-    const app = medley();
-
-    app.register(cookie);
+    const app = makeApp();
 
     app.get('/', (req, res) => {
-      res
-        .clearCookie('a', {expires: new Date()})
-        .clearCookie('b', {maxAge: 3600, httpOnly: true, secure: true})
-        .send();
+      res.clearCookie('a', {expires: new Date()});
+      res.clearCookie('b', {maxAge: 3600, httpOnly: true, secure: true});
+      res.send();
     });
 
-    const res = await app.inject('/');
+    const res = await app.request('/');
     assert.deepStrictEqual(res.headers['set-cookie'], [
       'a=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT',
       'b=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure',
@@ -114,9 +112,7 @@ describe('res.clearCookie()', () => {
 describe('res.signCookie() and req.unsignCookie()', () => {
 
   it('should sign and unsign cookies', async () => {
-    const app = medley();
-
-    app.register(cookie, {secret: 'tobiiscool'});
+    const app = makeApp({secret: 'tobiiscool'});
 
     app.get('/', (req, res) => {
       const signed = res.signCookie('hello');
@@ -128,8 +124,8 @@ describe('res.signCookie() and req.unsignCookie()', () => {
       res.send('success');
     });
 
-    const res = await app.inject('/');
-    assert.strictEqual(res.payload, 'success');
+    const res = await app.request('/');
+    assert.strictEqual(res.body, 'success');
   });
 
 });
